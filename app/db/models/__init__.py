d73090ce8c0e645b4b857a7187303310c7f5f76b
Jisu Hong <jaylovegood@snu.ac.kr>
2025-11-23 00:49:55 +0900

    calls, call_messages 테이블 추가

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -2,6 +2,8 @@
 from app.db.models.user import User
 from app.db.models.elder import Elder
 from app.db.models.call_schedule import CallSchedule
+from app.db.models.call import Call
+from app.db.models.call_message import CallMessage
 
-__all__ = ["User", "Elder", "CallSchedule"]
+__all__ = ["User", "Elder", "CallSchedule", "Call", "CallMessage"]
 
